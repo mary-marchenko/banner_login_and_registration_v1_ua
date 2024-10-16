5c6920f8f3401ed53dbd5fafed839ce9ac6326a7
Mari <mari@MacBook-Pro.local>
2024-10-16 13:15:57 +0300

    first commit

---
+++ front/js/main.js
@@ -0,0 +1,8 @@
+(function init100vh(){
+    function setHeight() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setHeight();
+    window.addEventListener('resize', setHeight);
+})();
